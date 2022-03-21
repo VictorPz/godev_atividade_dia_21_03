cdbaf5922c7874cf3a20f573f04f85581a0bf150
Victor Pizetta <joao.victor07.br2@gmail.com>
2022-03-21 18:50:24 -0300

    Initial Commit

---
+++ consumo_api_aula_21_03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  consumo_api_aula_21_03
+//
+//  Created by Victor Pizetta on 21/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
